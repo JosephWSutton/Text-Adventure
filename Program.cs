5e6a67586034836a897e73149b5fa33e636c2921
Joseph Sutton <jsutton1000@gmail.com>
2020-10-22 17:20:37 -0500

    see if this works

--- Program.cs
+++ Program.cs
@@ -18,21 +18,21 @@ namespace TheVagabondsDirectiveToTheUniverse
             Console.WriteLine("Ahh, well at least you rembember that!");
 
             Console.WriteLine("Do you happen to recall that we are currently running\n from " +
-                "the blorgon army and they are less then two clamshells behind us????\n " +
+                "the Blorgon army and they are less then two clamshells behind us????\n " +
                 "Well by the look on your face I daresay not.\nPRESS ENTER TO CONTINUE");
 
             Console.ReadLine();
 
-            Console.WriteLine($"Well heres the gist of it {playerName}, \n" +
-                $"the blorgon empire does not look favorably on vagabonds such as ourselves.\n" +
+            Console.WriteLine($"Well here is the gist of it {playerName}, \n" +
+                $"The Blorgon empire does not look favorably on vagabonds such as ourselves.\n" +
                 $"Their distaste reached it's pinnacle when you decided the Blorgon princess was\n" +
-                $"in need of some... extra-curricular fun, which is strongly reccomended against in chapter\n" +
-                $"four section eleven of the directive! Oh lord you probably don't even remember that. Do you remember???|n" +
+                $"in need of some... extra-curricular romping, which YOU SHOULD KNOW is strongly reccomended against in chapter\n" +
+                $"four, section eleven, paragraph six of the directive! Oh lord you probably don't even remember that. Do you???|n" +
                 $"PRESS y FOR YES, n FOR NO ");// game instructions
 
             string key = Console.ReadKey().Key.ToString();
 
-            if (key.ToLower() == "y")
+            if (key.ToLower() == "y") //Directive explanation
                 Console.WriteLine("Oh great, the Blorgon army is not scary even though they will disembowel us\n" +
                     " on sight, and I have plenty of time to explain the most important tool we have to you");
 
@@ -40,7 +40,7 @@ namespace TheVagabondsDirectiveToTheUniverse
                 Console.WriteLine("Well I don't believe you trashbag, im sorry, {playerName}, and I will explain anyway");
             else
                 Console.WriteLine("Did you forget how to use a keyboard too, you bumbling lunatic??\n" +
-                    "press y for yes or n for no!");
+                    "press y for yes or n for no!")
 
 
         }
